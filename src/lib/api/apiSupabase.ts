124c135d386fe8d109c480f6b307d47c95d7f2f6
Obed Cabanzo <78177466+ObedCabanzo@users.noreply.github.com>
2025-02-19 01:51:58 -0500

    Environment fixes

--- src/lib/api/apiSupabase.ts
+++ src/lib/api/apiSupabase.ts
@@ -1,6 +1,7 @@
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from "../../data/environment";
 import { createClient } from "@supabase/supabase-js";
 
+
 const supabase = createClient(
   SUPABASE_URL ? SUPABASE_URL : "",
   SUPABASE_ANON_KEY ? SUPABASE_ANON_KEY : ""
