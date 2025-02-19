c505cc90cee551348cadce01fec7ae9d1d5936ee
Obed Cabanzo <78177466+ObedCabanzo@users.noreply.github.com>
2025-02-19 01:15:47 -0500

    environment fixes

--- src/data/environment.js
+++ src/data/environment.js
@@ -1,5 +1,5 @@
-export const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
-export const AMPLITUDE_API_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY;
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ;
-export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ;
+export const API_HOST = process.env.API_HOST;
+export const AMPLITUDE_API_KEY = process.env.AMPLITUDE_API_KEY;
+export const SUPABASE_URL = process.env.SUPABASE_URL ;
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ;
 export const isProduction = process.env.NODE_ENV === 'production';
